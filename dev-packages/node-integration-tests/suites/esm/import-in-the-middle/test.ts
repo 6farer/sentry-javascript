e190793e71b9dee793207a16666d92c51f96b4d3
Luca Forstner <luca.forstner@sentry.io>
2024-12-19 14:14:17 +0100

    feat(node)!: Remove fine grained `registerEsmLoaderHooks` option and `addOpenTelemetryInstrumentation()` (#14606)

--- dev-packages/node-integration-tests/suites/esm/import-in-the-middle/test.ts
+++ dev-packages/node-integration-tests/suites/esm/import-in-the-middle/test.ts
@@ -8,7 +8,7 @@ afterAll(() => {
 });
 
 conditionalTest({ min: 18 })('import-in-the-middle', () => {
-  test('onlyIncludeInstrumentedModules', () => {
+  test('should only instrument modules that we have instrumentation for', () => {
     const result = spawnSync('node', [join(__dirname, 'app.mjs')], { encoding: 'utf-8' });
     expect(result.stderr).not.toMatch('should be the only hooked modules but we just hooked');
   });
